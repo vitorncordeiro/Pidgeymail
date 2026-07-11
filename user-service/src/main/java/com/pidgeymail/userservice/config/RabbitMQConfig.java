d8574a682020cc96f3802742f65bad4878933488
vitornc <vitornc31@gmail.com>
2026-07-10 22:37:46 -0300

    feat: add user service microsservice initial build with RabbitMQConfiguration class

---
+++ user-service/src/main/java/com/pidgeymail/userservice/config/RabbitMQConfig.java
@@ -0,0 +1,20 @@
+package com.pidgeymail.userservice.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.JacksonJsonMessageConverter;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String EXCHANGE = "user.exchange";
+    public static final String ROUTING_KEY_USER_CREATED = "user.created";
+    @Bean
+    public JacksonJsonMessageConverter messageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+    @Bean
+    public TopicExchange topicExchange(){
+        return new TopicExchange(EXCHANGE, true, false);
+    }
+}
